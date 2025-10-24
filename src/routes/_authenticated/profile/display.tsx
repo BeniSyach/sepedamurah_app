e884974c0301c210d55f0debdccfbc4f27217732
BeniSyach <benisyach32@gmail.com>
2025-10-24 23:49:34 +0700

    update

--- src/routes/_authenticated/profile/display.tsx
+++
@@ -1,6 +0,0 @@
-import { createFileRoute } from '@tanstack/react-router'
-import { SettingsDisplay } from '@/features/profile/display'
-
-export const Route = createFileRoute('/_authenticated/profile/display')({
-  component: SettingsDisplay,
-})
--- src/routes/_authenticated/profile/notifications.tsx
+++
@@ -1,6 +0,0 @@
-import { createFileRoute } from '@tanstack/react-router'
-import { SettingsNotifications } from '@/features/profile/notifications'
-
-export const Route = createFileRoute('/_authenticated/profile/notifications')({
-  component: SettingsNotifications,
-})
