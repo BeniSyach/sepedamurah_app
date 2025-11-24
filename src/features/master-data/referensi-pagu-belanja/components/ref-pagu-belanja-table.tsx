0f1bb5758a182fb05ebf70759c362f307cff5b65
BeniSyach <benisyach32@gmail.com>
2025-11-24 16:25:59 +0700

    update

--- src/features/master-data/referensi-pagu-belanja/components/ref-pagu-belanja-table.tsx
+++ src/features/master-data/referensi-pagu-belanja/components/ref-pagu-belanja-table.tsx
@@ -55,8 +55,8 @@ export function ReferensiPaguBelanjaTable({
   const [sorting, setSorting] = useState<SortingState>([])
 
   const {
-    columnFilters,
-    onColumnFiltersChange,
+    globalFilter,
+    onGlobalFilterChange,
     pagination,
     onPaginationChange,
     ensurePageInRange,
@@ -64,13 +64,11 @@ export function ReferensiPaguBelanjaTable({
     search,
     navigate,
     pagination: { defaultPage: 1, defaultPageSize: 10 },
-    columnFilters: [
-      {
-        columnId: 'rekening.nm_rekening',
-        searchKey: 'rekening.nm_rekening',
-        type: 'string',
-      },
-    ],
+    globalFilter: {
+      enabled: true,
+      key: 'search',
+      trim: false,
+    },
   })
 
   const totalRows = meta?.total ?? data.length
@@ -86,21 +84,16 @@ export function ReferensiPaguBelanjaTable({
       sorting,
       pagination,
       rowSelection,
-      columnFilters,
+      globalFilter,
       columnVisibility,
     },
-    enableRowSelection: true,
     onPaginationChange,
-    onColumnFiltersChange,
+    onGlobalFilterChange,
     onRowSelectionChange: setRowSelection,
     onSortingChange: setSorting,
     onColumnVisibilityChange: setColumnVisibility,
     getPaginationRowModel: getPaginationRowModel(),
     getCoreRowModel: getCoreRowModel(),
-    getFilteredRowModel: getFilteredRowModel(),
-    getSortedRowModel: getSortedRowModel(),
-    getFacetedRowModel: getFacetedRowModel(),
-    getFacetedUniqueValues: getFacetedUniqueValues(),
   })
 
   useEffect(() => {
@@ -109,12 +102,7 @@ export function ReferensiPaguBelanjaTable({
 
   return (
     <div className='space-y-4 max-sm:has-[div[role="toolbar"]]:mb-16'>
-      <DataTableToolbar
-        table={table}
-        searchPlaceholder='Search Pagu Belanja...'
-        searchKey='rekening.nm_rekening'
-        filters={[]}
-      />
+      <DataTableToolbar table={table} searchPlaceholder='Cari...' />
 
       <div className='overflow-hidden rounded-md border'>
         <Table>
