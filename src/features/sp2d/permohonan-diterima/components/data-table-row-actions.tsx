17dceb5b918dd0037c7bdb44bb8e275d7a373299
BeniSyach <benisyach32@gmail.com>
2025-11-27 11:14:23 +0700

    update sp2d terima untuk periksa walaupun sudah kirim berkas

--- src/features/sp2d/permohonan-diterima/components/data-table-row-actions.tsx
+++ src/features/sp2d/permohonan-diterima/components/data-table-row-actions.tsx
@@ -56,7 +56,7 @@ export function DataTableRowActions({ row }: DataTableRowActionsProps) {
             </>
           )}
 
-          {levelAkses !== 'Bendahara' && !sudahDikirim && (
+          {levelAkses !== 'Bendahara' && (
             <>
               <DropdownMenuItem
                 onClick={() => {
