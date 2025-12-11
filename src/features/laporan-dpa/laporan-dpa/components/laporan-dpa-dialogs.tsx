11fcc3ff441c5ada0ac808e571207d662927d636
BeniSyach <benisyach32@gmail.com>
2025-12-11 09:32:40 +0700

    update dpa pdf

--- src/features/laporan-dpa/laporan-dpa/components/laporan-dpa-dialogs.tsx
+++ src/features/laporan-dpa/laporan-dpa/components/laporan-dpa-dialogs.tsx
@@ -52,7 +52,10 @@ export function UsersDialogs() {
         const url = window.URL.createObjectURL(new Blob([response.data]))
         const link = document.createElement('a')
         link.href = url
-        link.setAttribute('download', currentRow.dpa?.nm_dpa ?? 'tes')
+        link.setAttribute(
+          'download',
+          `${currentRow.dpa?.nm_dpa ?? 'laporan'}.pdf`
+        )
         document.body.appendChild(link)
         link.click()
         link.remove()
